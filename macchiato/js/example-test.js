470d3e42dba2ccb529095350293b076c746c4ac5
VoQn <VoQn.tyrantist@gmail.com>
2012-06-12 06:25:34 +0900

    add :: demo page ( macchiato )

---
+++ macchiato/js/example-test.js
@@ -0,0 +1,19 @@
+// user's tests
+Macchiato.stock({
+  'number x, y => x + y == y + x' :
+    arbitrary( 'number', 'number' ).property( function( x, y ){
+        return x + y == y + x;
+  }),
+  'string str => str == true' :
+    arbitrary( 'string' ).property( function( str ){
+        return !!str;
+  }),
+  'integer x, y => div(x, y) * y + (x % y) == x' :
+    arbitrary( 'integer', 'integer' ).property( function( x, y ){
+        return where( [ y != 0 ], function( x, y ){
+          var n = Math.floor( x / y ), d = Math.floor( x % y );
+          return n * y + d == x;
+        });
+  })
+});
+
